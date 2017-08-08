437f158df886b24e06c1b99896d05961259abd45
Morgan LaCouture <morganlea18@gmail.com>
2017-08-07 17:32:31 -0700

    added loops

--- app.js
+++ app.js
@@ -11,23 +11,29 @@ var pdxAirport= {
 
         var cookieArray = [];
 
-    for (var i = 14; i > 0; i--) {
 
     cookieArray.push (Math.floor( this.customerPerHour() * this.avgCustomer ));
 
-    }
-
+   
     return cookieArray
 
     },
 
+
     addToDom: function () {
+
+        var timeArray = ['6 am : ', '7 am : ', '8 am : ', '9 am : ', '10 am : ', '11 am : ', '12 pm : ', '1 pm : ', 
+                    '2 pm : ', '3 pm : ', '4 pm : ', '5 pm : ', '6 pm : ', '7 pm : ', '8 pm : '];
+
+        for ( var i = 0; i < 15; i++){
+
    var airport1= document.getElementById( 'airport' );
     var aLi= document.createElement( 'li' );
-    aLi.innerHTML= '6am: 163 cookies';
+    aLi.innerHTML= timeArray[i] + this.totalCookiesPH();
     airport1.appendChild( aLi );
-
+        }
     }
+
 
     
 };
