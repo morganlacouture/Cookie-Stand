678ed68d9b0c1697c87747f3e52dc05651e0db82
Morgan LaCouture <morganlea18@gmail.com>
2017-08-09 13:36:39 -0700

    added css borders to table and centered/colored

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ Store.prototype.sum = function () {
 }
 
 Store.prototype.addToDom = function () {
-   
+
 
     var table = document.getElementById('stores');
     var newRow = document.createElement('tr');
@@ -54,11 +54,11 @@ Store.prototype.addToDom = function () {
     }
 
 }
-var pdxAirport = new Store('airport', 23, 65, 6.3);
-var pioneer = new Store('pioneer', 3, 24, 1.2);
-var powell = new Store('powell', 11, 38, 3.7);
-var john = new Store('johns', 11, 38, 3.7);
-var waterfront = new Store('waterfront', 2, 16, 4.6);
+var pdxAirport = new Store(' PDX Airport', 23, 65, 6.3);
+var pioneer = new Store('Pioneer Square', 3, 24, 1.2);
+var powell = new Store('Powell', 11, 38, 3.7);
+var john = new Store('St. Johns', 11, 38, 3.7);
+var waterfront = new Store('Waterfront', 2, 16, 4.6);
 
 
 
